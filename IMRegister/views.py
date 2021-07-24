31c2fd474934861483ea556f104662e86079ce74
prompt2k2 <57393178+prompt2k2@users.noreply.github.com>
2021-07-25 00:21:23 +0100

    mailer

--- IMRegister/views.py
+++ IMRegister/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .forms import IMRegisterForms
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
 
 
 def IncidentForm(request):
@@ -9,3 +11,19 @@ def IncidentForm(request):
     context['form'] = IMRegisterForms()    
     return render(request, 'IMRegister/incident.html', context)
     
+def sendEmail(request):
+    
+    if request.method == 'POST':
+        form = IMRegisterForms(request.POST)
+        if form.is_valid():
+            subject = "Incident Report"
+            body = {form}
+            message = "\n".join(body.values())
+            
+            try:
+                send_mail(subject, message, 'prompt2k2@gmail.com', ['prompt2k2@gmail.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid Header Found')
+            
+    form = IMRegisterForms()
+    return render(request, 'IMRegister/incident.html', {'form':form})
