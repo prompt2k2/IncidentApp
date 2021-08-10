4a9199ab1cb689d0d945d61e38a9b7d06fdd9e3a
prompt2k2 <57393178+prompt2k2@users.noreply.github.com>
2021-08-10 19:36:51 +0100

    upload assets

--- IncidentApp/settings.py
+++ IncidentApp/settings.py
@@ -129,13 +129,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static')),
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'assets')),
 
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
