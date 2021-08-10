1a21c2d886db73accf335fd4930fedfc56ad9318
prompt2k2 <57393178+prompt2k2@users.noreply.github.com>
2021-08-10 20:16:52 +0100

    heh

--- IncidentApp/wsgi.py
+++ IncidentApp/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IncidentApp.settings')
 
 application = get_wsgi_application()
+
+WEBSITE_PORT = 8000
