7be6922e72746c0046e31813dc580972ad3b0a26
SSAnalyticsGit <86230494+SSAnalyticsGit@users.noreply.github.com>
2021-08-19 14:39:39 +0100

    Update settings.py

--- IncidentApp/settings.py
+++ IncidentApp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 EMAIL_HOST = 'smtp.office365.com'
 EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
