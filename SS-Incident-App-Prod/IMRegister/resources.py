365689ac3b6be8cae7ff6bf00b6a2f553fcdf505
prompt2k2 <prompt2k2@gmail.com>
2021-11-25 09:51:53 +0100

    Completed v3

--- SS-Incident-App-Prod/IMRegister/resources.py
+++ SS-Incident-App-Prod/IMRegister/resources.py
@@ -1,23 +1,51 @@
 from import_export import resources
 from .models import Incident
 
+from io import BytesIO
+from django.http import HttpResponse
+from django.template.loader import get_template
+
+from xhtml2pdf import pisa
+import os
+from django.conf import settings
+from django.contrib.staticfiles import finders
+
 class IncidentResource(resources.ModelResource):
     class Meta:
         model = Incident
         
         
         
-from io import BytesIO
-from django.http import HttpResponse
-from django.template.loader import get_template
 
-from xhtml2pdf import pisa
+
+def link_callback(uri, rel):
+    # use short variable names
+    sUrl = settings.STATIC_URL      # Typically /static/
+    sRoot = settings.STATIC_ROOT    # Typically /home/userX/project_static/
+    mUrl = settings.MEDIA_URL       # Typically /static/media/
+    mRoot = settings.MEDIA_ROOT     # Typically /home/userX/project_static/media/
+
+    # convert URIs to absolute system paths
+    if uri.startswith(mUrl):
+        path = os.path.join(mRoot, uri.replace(mUrl, ""))
+    elif uri.startswith(sUrl):
+        path = os.path.join(sRoot, uri.replace(sUrl, ""))
+
+    # make sure that file exists
+    if not os.path.isfile(path):
+        raise Exception('media URI must start with %s or %s' % (sUrl, mUrl))
+    return path
+
 
 def render_to_pdf(template_src, context_dict={}):
     template = get_template(template_src)
     html  = template.render(context_dict)
+    
+    
     result = BytesIO()
-    pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), result)
+    pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8", "ISO-8859-1")), result, link_callback=link_callback)
+    
+   
     
     
     if not pdf.err:
