3d70939db9561a333e50531d2c94017d7a21a99b
prompt2k2 <prompt2k2@gmail.com>
2021-12-02 12:58:32 +0100

    Version Start X

--- SS-Incident-App-Prod/IMRegister/resources.py
+++ SS-Incident-App-Prod/IMRegister/resources.py
@@ -19,22 +19,38 @@ class IncidentResource(resources.ModelResource):
 
 
 def link_callback(uri, rel):
-    # use short variable names
-    sUrl = settings.STATIC_URL      # Typically /static/
-    sRoot = settings.STATIC_ROOT    # Typically /home/userX/project_static/
-    mUrl = settings.MEDIA_URL       # Typically /static/media/
-    mRoot = settings.MEDIA_ROOT     # Typically /home/userX/project_static/media/
+    
+    
+    # result = finders.find(uri)
+    # if result:
+    #     if not isinstance(result, (list, tuple)):
+    #         result = [result]
+    #     result = list(os.path.realpath(path) for path in result)
+    #     path = result[0]
+    
+    # else:
+    #     # use short variable names
+        sUrl = settings.STATIC_URL      # Typically /static/
+        sRoot = settings.STATIC_ROOT    # Typically /home/userX/project_static/
+        mUrl = settings.MEDIA_URL       # Typically /static/media/
+        mRoot = settings.MEDIA_ROOT     # Typically /home/userX/project_static/media/
+        bRoot = settings.BASE_DIR       #Project's base directory
 
-    # convert URIs to absolute system paths
-    if uri.startswith(mUrl):
-        path = os.path.join(mRoot, uri.replace(mUrl, ""))
-    elif uri.startswith(sUrl):
-        path = os.path.join(sRoot, uri.replace(sUrl, ""))
+        # convert URIs to absolute system paths
+        if uri.startswith(mUrl):
+            path = os.path.join(mRoot, uri.replace(mUrl, ""))
+        elif uri.startswith(sUrl):
+            path = os.path.join(sRoot, uri.replace(sUrl, ""))
+        
+        else:
+            
+            return uri
+            #return os.path.join(bRoot, '../', uri)#uri
 
     # make sure that file exists
-    if not os.path.isfile(path):
-        raise Exception('media URI must start with %s or %s' % (sUrl, mUrl))
-    return path
+        if not os.path.isfile(path):
+            raise Exception('media URI must start with %s or %s' % (sUrl, mUrl))
+        return path
 
 
 def render_to_pdf(template_src, context_dict={}):
@@ -52,3 +68,5 @@ def render_to_pdf(template_src, context_dict={}):
         #return HttpResponse(result.getvalue(), content_type='application/pdf') #return HTTP response.
         return result.getvalue() # returns the as pdf file.
     return None
+    
+    
--- SS-Incident-App-Prod/IncidentApp/settings.py
+++ SS-Incident-App-Prod/IncidentApp/settings.py
@@ -152,15 +152,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/assets/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'assets')),
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'IncidentApp/staticfiles')#C:/Users/IT Manager/DEV/SS-Incident-App-Prod/SS-Incident-App-Prod/static
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'IncidentApp/media')
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
